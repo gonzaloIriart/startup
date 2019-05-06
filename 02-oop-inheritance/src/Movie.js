95bc4ed5b9941ad0fcf024616a59bde5e3ed18b1
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-06 18:24:54 -0300

    ex 2

--- 02-oop-inheritance/src/Movie.js
+++ 02-oop-inheritance/src/Movie.js
@@ -7,17 +7,32 @@ class Movie extends EventEmitter{
     }
 
     play(){ 
-        super.on('play',()=>{
-            console.log(this.name + ' is playing...')
-        })
-            
+        if(!this.events['play']){
+            super.on('play',()=>{
+                console.log(this.name + ' is playing...')
+            })
+        }
+       
+        super.emit('play');
         }
 
     pause(){
-        console.log('movie is paused')
-    }
+        if(!this.events['pause']){
+            super.on('pause',()=>{
+                console.log(this.name + ' is in pause...')
+            })
+        }
+       
+        super.emit('pause');
+        }
 
     resume(){
-        super.emit('play');
-    }
+        if(!this.events['resume']){
+            super.on('resume',()=>{
+                console.log(this.name + ' is resuming...')
+            })
+        }
+       
+        super.emit('resume');
+        }
 }
--- 02-oop-inheritance/src/index.js
+++ 02-oop-inheritance/src/index.js
@@ -3,4 +3,7 @@ const starWars = new Movie('Star wars: a new hope',1977,125)
 const guardians = new Movie('Guardians of the Galaxy',2014,125)
 
 interstellar.play();
+starWars.play();
+guardians.play();
+interstellar.pause();
 interstellar.resume();
