58011dbb2bd2f3ed6e90ff37ac1a60db57975d97
GonzaloIriart <gonzaloiriart@gmail.com>
2019-05-21 18:12:10 -0300

    exercise 6

--- 05-UI-ReactJS/movie-app/src/components/App/App.js
+++ 05-UI-ReactJS/movie-app/src/components/App/App.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './App.css';
 import NewMovieForm from '../NewMovieForm/NewMovieForm';
 import MovieList from '../MovieList/MovieList';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+const newMovieForm = () => {
+  return <NewMovieForm />
+}
+
+const movieList = () =>{
+  return <MovieList />
+  
+}
 
 class App extends React.Component {
  
@@ -13,8 +23,17 @@ render(){
       <header className="App-header">
         Movie List App
       </header>
-      <NewMovieForm />
-      <MovieList  />  
+     
+      <BrowserRouter>
+      <nav>
+        <Link className="navlink" to='/'>Add New Movie</Link>
+        <Link className="navlink" to='/movielist'>Movie List</Link>
+      </nav>
+        <div>
+          <Route path='/' exact component = {newMovieForm}/>
+          <Route path='/movielist' component = {movieList}/>
+        </div>
+      </BrowserRouter>
     </div>
     );
   }
